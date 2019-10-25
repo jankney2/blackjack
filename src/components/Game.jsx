32310b01bcbbea95375ca80ba12c3976b949e12e
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-10-25 11:07:13 -0600

    working on player logic

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,13 +5,12 @@ import Card from "./Card";
 const Game = () => {
   const [playerHand, updatePlayerHand] = useState([]);
   const [dealerHand, updateDealerHand] = useState([]);
-  const [playerHandVal, updatePlayerHandVal]=useState(0)
-  const [dealerHandVal, updateDealerHandVal]=useState(0)
+  const [playerHandVal, updatePlayerHandVal] = useState(0);
+  const [dealerHandVal, updateDealerHandVal] = useState(0);
   const [deck, d] = useState(cards);
   const [turn, switchTurn] = useState("player");
- const [playerScore, updatePlayerScore]=useState(0)
- const [dealerScore, updateDealerScore]=useState(0)
-
+  const [playerScore, updatePlayerScore] = useState(0);
+  const [dealerScore, updateDealerScore] = useState(0);
 
   const deal = () => {
     //generate 4 random cards from the deck
@@ -23,32 +22,55 @@ const Game = () => {
 
     updateDealerHand([dc1, dc2]);
     updatePlayerHand([p1c1, p1c2]);
-    
-    let allCards=[p1c1, p1c2, dc1, dc2]
 
-    allCards.forEach(el=>{
-        if(el.value==='J'||el.value==='K'||el.value==='Q'){
-            el.faceValue=el.value
-            el.value=10
-        }
+    let allCards = [p1c1, p1c2, dc1, dc2];
 
-        if(el.value==='A'){
-            el.value=11
-            el.faceValue='A'
-        }
-    })
+    allCards.forEach(el => {
+      if (el.value === "J" || el.value === "K" || el.value === "Q") {
+        el.faceValue = el.value;
+        el.value = 10;
+      }
 
+      if (el.value === "A") {
+        el.value = 11;
+        el.faceValue = "A";
+      }
+    });
 
-
-    let initPlayerScore=p1c1.value+p1c2.value
-    let initiDealerScore=dc1.value+dc2.value
-    updatePlayerHandVal(initPlayerScore)
-    updateDealerHandVal(initiDealerScore)
-
+    let initPlayerScore = p1c1.value + p1c2.value;
+    let initiDealerScore = dc1.value + dc2.value;
+    updatePlayerHandVal(initPlayerScore);
+    updateDealerHandVal(initiDealerScore);
   };
 
+  const hit = () => {
+    if (turn === "player") {
+      console.log("hit if");
+      let newCard = deck[Math.round(Math.random() * 52)];
+
+      if (
+        newCard.value === "J" ||
+        newCard.value === "K" ||
+        newCard.value === "Q"
+      ) {
+        newCard.faceValue = newCard.value;
+        newCard.value = 10;
+      }
+
+      if (newCard.value === "A") {
+        newCard.value = 11;
+        newCard.faceValue = "A";
+      }
+
+      console.log(newCard, 'newCard')
+      let newVal = playerHandVal + newCard.value;
+      console.log('newVal', newVal)
+      updatePlayerHandVal(newVal);
+      updatePlayerHand([...playerHand, newCard]);
+    }
+  };
   useEffect(() => {
-    deal()
+    deal();
   }, []);
 
   //using the index as the key in a .map is bad form, but the objects i'm using for the cards don't have an Id, so for simplicity's sake I'm using the index.
@@ -62,27 +84,32 @@ const Game = () => {
 
   return (
     <div>
-      <button
-        onClick={() => {
-          console.log(playerHand, dealerHand);
-        }}
-      >
-        click for state
-      </button>
-
       <div className="dealerHand">{dealerCardMapper}</div>
 
       <div className="message">
-        <h2>What would you like to do?</h2>
+        <h2>Player, What would you like to do?</h2>
 
-        <button>Hit</button>
-        <button>Hold</button>
+        <button
+          onClick={() => {
+            hit();
+          }}
+        >
+          Hit
+        </button>
+        <button
+          onClick={() => {
+            switchTurn("dealer");
+          }}
+        >
+          Hold
+        </button>
       </div>
 
       <div className="playerHand">
         Player Hand
         {playerCardMapper}
         <p>Player hand Val: {playerHandVal}</p>
+        <p>Player Score:{playerScore}</p>
       </div>
     </div>
   );
