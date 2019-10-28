e6635948e49fb7c10c81eb749288a359ada42448
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-10-28 11:13:12 -0600

    rework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+
 import Game from './components/Game'
-import Test from './components/Test'
 
 function App() {
   return (
     <div>
-<Test />
+<Game />
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,29 +1,19 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import cards from "../deck";
 import Card from "./Card";
 
-
 export default class Game extends Component {
-  state={
-    playerHand:[], 
-    dealerHand:[], 
-    playerHandVal:0, 
-    dealerHandVal:0, 
-    deck:cards, 
-    playerScore:0, 
-    dealerScore:0, 
-  }
-  
-//     let [playerHand, updatePlayerHand] = useState([]);
-//   let [dealerHand, updateDealerHand] = useState([]);
-//   let [playerHandVal, updatePlayerHandVal] = useState(0);
-//   let [dealerHandVal, updateDealerHandVal] = useState(0);
-//   let [deck] = useState(cards);
-//   let [playerScore, updatePlayerScore] = useState(0);
-//   let [dealerScore, updateDealerScore] = useState(0);
+  state = {
+    playerHand: [],
+    dealerHand: [],
+    playerHandVal: 0,
+    dealerHandVal: 0,
+    deck: cards,
+    playerScore: 0,
+    dealerScore: 0
+  };
 
-
-deal = () => {
+  deal = () => {
     //generate 4 random cards from the deck
 
     let p1c1 = this.state.deck[Math.floor(Math.random() * 52)];
@@ -34,147 +24,138 @@ deal = () => {
     let initDealerVal = dc1.value + dc2.value;
 
     this.setState({
-        dealerHand:[dc1, dc2],
-        playerHand:[p1c1, p1c2],
-        dealerHandVal:initDealerVal, 
-        playerHandVal:initPlayerVal
-    })
-    console.log(this.state.playerHandVal, this.state.dealerHandVal, 'init values')
+      dealerHand: [dc1, dc2],
+      playerHand: [p1c1, p1c2],
+      dealerHandVal: initDealerVal,
+      playerHandVal: initPlayerVal
+    });
+    console.log(
+      this.state.playerHandVal,
+      this.state.dealerHandVal,
+      "init values"
+    );
     if (this.state.playerHandVal === 21 && this.state.dealerHandVal === 21) {
       alert("push!");
 
-
-
-        this.setState({
-            playerScore:this.state.playerScore+1, 
-            dealerScore:this.state.dealerScore+1
-        })
+      this.setState({
+        playerScore: this.state.playerScore + 1,
+        dealerScore: this.state.dealerScore + 1
+      });
       this.deal();
     }
 
     if (initPlayerVal === 21) {
-        this.setState({
-            playerScore:this.state.playerScore+2
-        })
+      this.setState({
+        playerScore: this.state.playerScore + 2
+      });
 
       this.deal();
     }
     if (initDealerVal === 21) {
-        this.setState({
-            dealerScore:this.state.dealerScore+2
-        })
+      this.setState({
+        dealerScore: this.state.dealerScore + 2
+      });
 
       this.deal();
     }
   };
 
-
-
-   getNewCard = () => {
+  getNewCard = () => {
     let newCard = this.state.deck[Math.floor(Math.random() * 52)];
-   
+
     return newCard;
   };
-   dealerHit =  () => {
-      let newCard = this.getNewCard();
-      let newVal = this.state.dealerHandVal + newCard.value;
+  dealerHit = () => {
+    let newCard = this.getNewCard();
+    let newVal = this.state.dealerHandVal + newCard.value;
 
-      this.setState({
-          dealerHandVal:newVal
-      })
- 
-    this.winLogic(this.state.dealerHandVal)
-    
     this.setState({
-        dealerHand:[...this.state.dealerHand, newCard]
-    })
+      dealerHandVal: newVal
+    });
 
+    this.winLogic(this.state.dealerHandVal);
 
-    
+    this.setState({
+      dealerHand: [...this.state.dealerHand, newCard]
+    });
+
     if (newVal < 17) {
-        this.hit("dealer");
-        this.winLogic(newVal)
-
+      this.hit("dealer");
+      this.winLogic(newVal);
     }
-
-
 
     if (this.state.playerHandVal > newVal && newVal >= 17) {
-
-    this.hit("dealer");
-      this.winLogic(newVal)
-
+      this.hit("dealer");
+      this.winLogic(newVal);
     }
-
-
-    
   };
 
-   hit = async (type) => {
-    let newCard = this.getNewCard()
+  hit = async type => {
+    let newCard = this.getNewCard();
 
     if (type === "player") {
-        this.setState({
-            playerHand:[...this.state.playerHand, newCard]
-        })
-      let newVal = this.state.playerHandVal + newCard.value;
-      
       this.setState({
-          playerHandVal:newVal
-      })
+        playerHand: [...this.state.playerHand, newCard]
+      });
+      let newVal = this.state.playerHandVal + newCard.value;
 
-    //   if (newVal === 21) {
-    //     alert("you won the hand!");
-    //     updatePlayerScore(playerScore++);
-    //     deal();
-    //     return;
-    //   }
-
-    //   if (newVal > 21) {
-    //     alert("player bust :(");
-    //     updateDealerScore(++dealerScore);
-    //     deal();
-    //     return;
-    //   }
+      await this.setState({
+        playerHandVal: newVal
+      });
+      console.log(this.state.playerHandVal, "phv");
 
 
+        if (this.state.playerHandVal === 21) {
+          alert("you won the hand!");
+          this.setState({
+            playerScore:this.state.playerScore+1
+          })
+          this.deal();
+          return;
+        }
+
+        if (newVal > 21) {
+          alert("player bust :(");
+          this.setState({
+            dealerScore:this.state.dealerScore+1
+          })
+          this.deal();
+          return;
+        }
     }
 
     if (type === "dealer") {
       console.log("dealer hit fawiojfe");
 
-        this.dealerHit();
-
+      this.dealerHit();
     }
   };
 
-   winLogic = (dealer) => {
-
-
+  winLogic = dealer => {
     if (dealer > 21) {
       alert("dealer bust");
       this.setState({
-          playerScore:this.state.playerScore+1
-      })
+        playerScore: this.state.playerScore + 1
+      });
 
       this.deal();
       return;
     }
     if (dealer === 21) {
       alert("dealer Win");
-        this.setState({
-            dealerScore:this.state.dealerScore+2
-        })
+      this.setState({
+        dealerScore: this.state.dealerScore + 2
+      });
 
-    this.deal();
+      this.deal();
       return;
     }
     if (this.state.playerHandVal === dealer) {
       alert("dealer win");
 
       this.setState({
-          dealerScore:this.state.dealerScore+1
-      })
+        dealerScore: this.state.dealerScore + 1
+      });
       this.deal();
       return;
     }
@@ -183,95 +164,81 @@ deal = () => {
       if (dealer > this.state.playerHandVal) {
         alert("dealer win");
         this.setState({
-            dealerScore:this.state.dealerScore+1
-        })
+          dealerScore: this.state.dealerScore + 1
+        });
 
         this.deal();
         return;
       } else {
         alert("player win");
         this.setState({
-            playerScore:++this.state.playerScore
-        })
+          playerScore: this.state.playerScore + 1
+        });
         this.deal();
         return;
       }
     }
 
-    if(dealer>this.state.playerHandVal){
-        alert('dealer win')
-        this.setState({
-            dealerScore:++this.state.dealerScore
-        })
+    if (dealer > this.state.playerHandVal) {
+      alert("dealer win");
+      this.setState({
+        dealerScore: this.state.dealerScore + 1
+      });
 
-        this.deal()
-        return
+      this.deal();
+      return;
     }
   };
 
-componentDidMount() {
-    this.deal()
-}
+  componentDidMount() {
+    this.deal();
+  }
 
   //using the index as the key in a .map is bad form, but the objects i'm using for the cards don't have an Id, so for simplicity's sake I'm using the index.
 
-  render(){
+  render() {
     let playerCardMapper = this.state.playerHand.map((el, i) => {
-        return <Card suit={el.suit} key={i} val={el.value} />;
-      });
-    
-      let dealerCardMapper = this.state.dealerHand.map((el, i) => {
-        return <Card suit={el.suit} key={i} val={el.value} />;
-      });
-    
-      return (
-        <div>
-          <div className="hand">
-              <div className="cardHolder">
-            {dealerCardMapper}
-    
-              </div>
-            <p>dealer hand Val: {this.state.dealerHandVal}</p>
-            <p>Dealer Score:{this.state.dealerScore}</p>
-          </div>
-    
-          <div className="message">
-            <h2>Player, What would you like to do?</h2>
-    
-            <button
-              onClick={() => {
-                this.hit("player");
-              }}
-            >
-              Hit
-            </button>
-            <button
-              onClick={() => {
-                this.hit("dealer");
-              }}
-            >
-              Hold
-            </button>
-          </div>
-    
-          <div className="hand">
-            Player Hand
-    
-            <div className="cardHolder">
-            {playerCardMapper}
-    
-            </div>
-            <p>Player hand Val: {this.state.playerHandVal}</p>
-            <p>Player Score:{this.state.playerScore}</p>
-          </div>
-    
-    
-    
-    
+      return <Card suit={el.suit} key={i} val={el.value} />;
+    });
+
+    let dealerCardMapper = this.state.dealerHand.map((el, i) => {
+      return <Card suit={el.suit} key={i} val={el.value} />;
+    });
+
+    return (
+      <div>
+        <div className="hand">
+          <div className="cardHolder">{dealerCardMapper}</div>
+          <p>dealer hand Val: {this.state.dealerHandVal}</p>
+          <p>Dealer Score:{this.state.dealerScore}</p>
         </div>
-      );
+
+        <div className="message">
+          <h2>Player, What would you like to do?</h2>
+
+          <button
+            onClick={() => {
+              this.hit("player");
+            }}
+          >
+            Hit
+          </button>
+          <button
+            onClick={() => {
+              this.hit("dealer");
+            }}
+          >
+            Hold
+          </button>
+        </div>
+
+        <div className="hand">
+          Player Hand
+          <div className="cardHolder">{playerCardMapper}</div>
+          <p>Player hand Val: {this.state.playerHandVal}</p>
+          <p>Player Score:{this.state.playerScore}</p>
+        </div>
+      </div>
+    );
   }
-
-};
-
-
+}
